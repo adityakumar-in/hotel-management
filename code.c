d710f931c7a14965317d629ce595cfd380710b10
Aditya Kumar <93466706+adityakumar-in@users.noreply.github.com>
2021-11-26 10:21:04 +0530

    Added some key features

--- code.c
+++ code.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
 
 void main(){
-  
+    int option;
+
+    printf("Hotel Management System\n\n");
+
+    while(True){
+        printf("(Choose anyone from below)\n");
+        printf("1. New Customer\n2. Already a Customer\n");
+        scanf("%d", &option);
+        
+        if(option == 1){
+            
+    } 
 
 } 
